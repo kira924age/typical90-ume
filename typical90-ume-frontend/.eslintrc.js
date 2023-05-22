bcb33c5fd36ad657b25aca95b32d768d10ec3c73
kira924age <kira924age@gmail.com>
2023-05-22 20:50:08 +0900

    ci: 🎡 setting ESLint configuration
    add .eslint.js and eslint packages

---
+++ typical90-ume-frontend/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    'vue/setup-compiler-macros': true
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'standard-with-typescript'],
+  overrides: [],
+  // Vueを読み込めるようにする。eslint-plugin-vueが提供してくれている。
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    // VueをTypescriptで使う場合は指定する必要がある。vue-eslint-parser固有のオプション
+    parser: '@typescript-eslint/parser',
+    // ES Modulesを使ってコードを書く場合はmoduleを指定する必要がある。
+    sourceType: 'module',
+    ecmaVersion: 'latest',
+    project: './tsconfig.json'
+  },
+  plugins: ['vue'],
+  rules: {}
+}
