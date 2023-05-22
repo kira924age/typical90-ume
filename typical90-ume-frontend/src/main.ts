0db5378688ea0c4cd33bd3aeb063acf4084ca2c4
kira924age <kira924age@gmail.com>
2023-05-22 16:05:28 +0900

    Revert "ci: 🎡 setting ESLint"
    This reverts commit a4dbbd7fb66e57086444fccf2343c15d2bf4efae.

--- typical90-ume-frontend/src/main.ts
+++ typical90-ume-frontend/src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
-import TheApp from './the-app.vue'
+import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
 
-const app = createApp(TheApp)
+const app = createApp(App)
 
 app.use(router)
 
