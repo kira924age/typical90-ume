1b10e9ecc2a045e028f7afa4e6d58c0839accf40
kira924age <kira924age@gmail.com>
2023-06-02 20:40:56 +0900

    fix: 🐛 replace import component name

--- typical90-ume-frontend/src/views/home-view.vue
+++ typical90-ume-frontend/src/views/home-view.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-// import ProblemsTable from '@/features/problemsTable/problems-table.vue'
-import ProblemsTable from '@/features/problemsTable/new-problems-table.vue'
+import ProblemsTable from '@/features/problemsTable/problems-table.vue'
 </script>
 
 <template>
