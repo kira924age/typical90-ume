57e66a2630c7bd57ca1dc2a726ed9e5c57b8cbf3
kira924age <kira924age@gmail.com>
2023-06-23 02:17:56 +0900

    fix: 🐛 add files
    i forgot to git add

---
+++ typical90-ume-frontend/src/features/userStatus/components/climbing-chart.vue
@@ -0,0 +1,136 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { problemMap } from '../../problemsTable/composables/use-problems'
+import type Submission from '@/types/submission'
+
+const properties = defineProps<{
+  submissions?: Submission[] | undefined
+}>()
+
+type Series = [number, number]
+
+const stackAreaData = computed(() => {
+  const mp = new Map<number, Set<string>>()
+
+  mp.set(2, new Set<string>())
+  mp.set(3, new Set<string>())
+  mp.set(4, new Set<string>())
+  mp.set(5, new Set<string>())
+  mp.set(6, new Set<string>())
+  mp.set(7, new Set<string>())
+
+  const response = new Map<number, Array<Series>>()
+  response.set(2, [])
+  response.set(3, [])
+  response.set(4, [])
+  response.set(5, [])
+  response.set(6, [])
+  response.set(7, [])
+
+  if (properties.submissions === undefined) {
+    return response
+  }
+
+  for (const submission of properties.submissions) {
+    const problemId = submission.problem_id
+    const unixTime = submission.epoch_second * 1000
+    const result = submission.result
+
+    if (result !== 'AC') {
+      continue
+    }
+
+    const problem = problemMap.get(problemId)
+    if (problem === undefined) {
+      continue
+    }
+
+    const { star } = problem
+
+    const a = mp.get(Number(star))
+    if (a instanceof Set<string> && !a.has(problemId)) {
+      a.add(problemId)
+    }
+
+    for (let star = 2; star <= 7; star++) {
+      const b = response.get(Number(star))
+      if (b instanceof Array<Series>) {
+        b.push([unixTime, mp.get(star)?.size ?? 0])
+      }
+    }
+  }
+
+  return response
+})
+
+const series = computed(() => {
+  return Array.from({ length: 6 })
+    .fill(0)
+    .map((_, index) => {
+      return {
+        name: `星${7 - index}`,
+        data: stackAreaData.value.get(7 - index)
+      }
+    })
+})
+
+const chartOptions = {
+  chart: {
+    type: 'area',
+    height: 350,
+    stacked: true,
+    animations: {
+      enabled: false
+    }
+  },
+  colors: ['#C0C000', '#0000FF', '#00C0C0', '#008000', '#804000', '#808080'],
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'straight'
+  },
+  fill: {
+    type: 'solid'
+  },
+  legend: {
+    position: 'top',
+    horizontalAlign: 'left',
+    inverseOrder: true,
+    labels: {
+      colors: 'gray',
+      useSeriesColors: false
+    }
+  },
+  tooltip: {
+    inverseOrder: true
+  },
+  xaxis: {
+    type: 'datetime',
+    labels: {
+      style: {
+        colors: 'gray'
+      }
+    }
+  },
+  yaxis: {
+    labels: {
+      style: {
+        colors: 'gray'
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <div id="chart">
+    <apexchart type="area" height="350" :options="chartOptions" :series="series"></apexchart>
+  </div>
+</template>
+
+<style scoped lang="scss">
+::v-deep(.apexcharts-tooltip) {
+  color: black;
+}
+</style>
---
+++ typical90-ume-frontend/src/features/userStatus/components/difficulty-pie-chart.vue
@@ -0,0 +1,129 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const properties = defineProps({
+  star: {
+    type: Number,
+    required: true
+  },
+  ac: {
+    type: Number,
+    required: true
+  }
+})
+
+const numberOfProblem = new Map<number, number>([
+  [2, 10],
+  [3, 20],
+  [4, 14],
+  [5, 17],
+  [6, 14],
+  [7, 15]
+])
+
+const solvedRatio = computed(() => {
+  return `${String(properties.ac ?? 0)}/${String(numberOfProblem.get(properties.star) ?? 0)}`
+})
+
+const label = computed(() => {
+  if (properties.star === undefined) {
+    return '-'
+  }
+  return `星${properties.star}`
+})
+
+const colors = new Map<number, string>([
+  [2, '#808080'],
+  [3, '#804000'],
+  [4, '#008000'],
+  [5, '#00C0C0'],
+  [6, '#0000FF'],
+  [7, '#C0C000']
+])
+
+const color = computed(() => {
+  if (properties.star === undefined) {
+    return '#808080'
+  }
+  return colors.get(properties.star) ?? '#808080'
+})
+
+const labels = computed(() => {
+  return 'AC: ' + String(properties.ac)
+})
+
+const chartOptions = computed(() => {
+  return {
+    chart: {
+      height: 240,
+      type: 'radialBar',
+      animations: {
+        enabled: false
+      }
+    },
+    plotOptions: {
+      radialBar: {
+        hollow: {
+          size: '70%'
+        },
+        dataLabels: {
+          name: {
+            offsetY: -10,
+            show: true,
+            color: '#888',
+            fontSize: '18px'
+          },
+          value: {
+            color: '#888',
+            fontSize: '24px',
+            show: true,
+            formatter: (value: number) => {
+              return value.toFixed(1) + '%'
+            }
+          }
+        }
+      }
+    },
+    colors: [color.value],
+    stroke: {
+      lineCap: 'round'
+    },
+    labels: [labels.value]
+  }
+})
+
+const acRatio = computed(() => {
+  if (properties.ac === undefined) {
+    return 0
+  }
+  return (properties.ac / (numberOfProblem.get(properties.star) ?? 0)) * 100
+})
+
+const series = computed(() => {
+  return [acRatio.value]
+})
+</script>
+
+<template>
+  <div>
+    <apexchart height="240" type="radialBar" :options="chartOptions" :series="series"></apexchart>
+    <div class="pie-status-count">{{ solvedRatio }}</div>
+    <div class="pie-status-label">{{ label }}</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.pie-status-count {
+  font-size: 1.5rem;
+  font-weight: 500;
+  color: #888;
+  text-align: center;
+  padding-bottom: 0.5rem;
+}
+
+.pie-status-label {
+  font-size: 1.5rem;
+  font-weight: 500;
+  text-align: center;
+}
+</style>
