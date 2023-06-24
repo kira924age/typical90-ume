a2ee3958742de4660b6db53c0a8965a39da87fbd
kira924age <kira924age@gmail.com>
2023-06-24 09:54:27 +0900

    feat: 🎸 add scripts
    add scripts for build or deploy

---
+++ typical90-ume-backend/scripts/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -eu
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+BACKEND_ROOT_DIR=$SCRIPT_DIR/../
+
+cd $BACKEND_ROOT_DIR
+rm -rf node_modules
+
+npm install
+npx prisma generate
+npx prisma migrate deploy
+npx tsc
