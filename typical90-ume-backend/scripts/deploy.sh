94bd2ba9f79fbe6bd088e90aad0c74533832564f
Akira Kunishige <kira924age@users.noreply.github.com>
2023-06-26 03:08:43 +0900

    Update deploy.sh

--- typical90-ume-backend/scripts/deploy.sh
+++ typical90-ume-backend/scripts/deploy.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 BACKEND_ROOT_DIR=$SCRIPT_DIR/../
 
-pm2 start $BACKEND_ROOT_DIR/dist/index.js --name typical90-backend
+pm2 restart $BACKEND_ROOT_DIR/dist/index.js --name typical90-backend
