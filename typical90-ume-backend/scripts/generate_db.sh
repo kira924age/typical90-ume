76a8596ae0bac482415cf5d251d68989dd3333b0
Akira Kunishige <kira924age@users.noreply.github.com>
2023-06-26 07:05:28 +0900

    Update generate_db.sh

--- typical90-ume-backend/scripts/generate_db.sh
+++ typical90-ume-backend/scripts/generate_db.sh
@@ -3,11 +3,13 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR
 
-wget https://s3-ap-northeast-1.amazonaws.com/kenkoooo/submissions.csv.gz
-gzip -d submissions.csv.gz
-python3 generate_csv.py
-rm submissions.csv
+wget https://s3-ap-northeast-1.amazonaws.com/kenkoooo/submissions.csv.gz;
+gzip -d submissions.csv.gz;
+python3 generate_csv.py;
+rm submissions.csv;
 
-sqlite3 typical90.db < ./create_table.sql
-rm typical90_submissions.csv
-mv typical90.db ../db/
+sqlite3 new_typical90.db < ./create_table.sql;
+rm typical90_submissions.csv;
+
+scp new_typical90.db rider:/home/ubuntu/apps/typical90-ume/typical90-ume-backend/db/typical90.db
+rm new_typical90.db
