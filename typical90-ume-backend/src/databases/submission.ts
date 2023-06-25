2f7a7c01d5b66abb07a4f2d5b55e8af8ea64e556
kira924age <kira924age@gmail.com>
2023-06-26 02:46:36 +0900

    feat: 🎸 call atcoder problems API
    ✅ Closes: #21

--- typical90-ume-backend/src/databases/submission.ts
+++ typical90-ume-backend/src/databases/submission.ts
@@ -1,4 +1,18 @@
 import { PrismaClient } from "@prisma/client";
+import { getLatestSubmissionTime } from "./latest-submission-time";
+
+const fetch = require("node-fetch");
+
+/**
+ * Suspends the execution for a specified number of seconds.
+ * @param seconds - The number of seconds to sleep.
+ * @returns A promise that resolves after the specified number of seconds.
+ */
+const sleep = (seconds: number): Promise<void> => {
+  return new Promise<void>((resolve) => {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
 
 /**
  * Retrieves submissions for a given user.
@@ -18,6 +32,32 @@ export const readSubmissionsFromDb = async (userId: string) => {
         epoch_second: "desc",
       },
     });
+
+    let lastSubmissionTime = (await getLatestSubmissionTime()) ?? 0;
+    while (1) {
+      const url =
+        "https://kenkoooo.com/atcoder/atcoder-api/v3/user/submissions?user=" +
+        userId +
+        "&from_second=" +
+        String(lastSubmissionTime + 1);
+      const res = await fetch(url, {
+        headers: { "Accept-Encoding": "gzip" },
+      });
+
+      if (res.ok) {
+        const data = await res.json();
+        for (const submission of data) {
+          if (data.contest_id === "typical90") {
+            submissions.push(submission);
+          }
+        }
+        if (data.length !== 500) {
+          break;
+        }
+        lastSubmissionTime = data[data.length - 1].epoch_second;
+      }
+      sleep(1);
+    }
 
     return submissions;
   } catch (err) {
